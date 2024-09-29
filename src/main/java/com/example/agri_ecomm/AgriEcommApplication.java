a7331ea770c4ea5193fbb81b80303586187aaa5c
ivancliff029 <odekeivancliff@gmail.com>
2024-09-29 10:37:40 +0300

    Feature : Product catalog  feature

--- src/main/java/com/example/agri_ecomm/AgriEcommApplication.java
+++ src/main/java/com/example/agri_ecomm/AgriEcommApplication.java
@@ -2,9 +2,13 @@ package com.example.agri_ecomm;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.example.agri_ecomm", "com.example.productcatalog"})
+@EntityScan(basePackages = "com.example.productcatalog.model")
 @EnableJpaRepositories(basePackages = "com.example.productcatalog.repository")
 public class AgriEcommApplication {
 
